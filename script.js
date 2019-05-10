d0bec1a9adc0b9325a86b1e5dad30fc125a5ba40
ljelias <laura.j.elias@gmail.com>
2019-05-09 23:34:39 -0500

    works with one glitch of repeated entries

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
   //My web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCkw-0bhvwcf1h-BdEVopndb7GVAX4eYco",
     authDomain: "train-scheduler-9502f.firebaseapp.com",
     databaseURL: "https://train-scheduler-9502f.firebaseio.com",
@@ -8,79 +8,83 @@
     storageBucket: "train-scheduler-9502f.appspot.com",
     messagingSenderId: "719280534359",
     appId: "1:719280534359:web:e0dea3bd971f7196"
-  };
+};
 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var trainName = "--";
-  var destination = "--";
-  var firstTrain = "--";
-  var frequency = 0;
-  var nextArrival = "--";
-  var minutesAway = 0;
 
 //to display the current date and time in the header
 var currentDayTime = moment();
-    console.log(moment(currentDayTime).format("HH:mm"));
-    console.log(moment(currentDayTime).format("dddd, MMMM Do, YYYY"));
+    //console.log(moment(currentDayTime).format("HH:mm"));
+    //console.log(moment(currentDayTime).format("dddd, MMMM Do, YYYY"));
+$("#current-time").html(moment(currentDayTime).format("dddd, MMMM Do, YYYY ~ HH:mm"));
 
-    $("#current-time").html(moment(currentDayTime).format("dddd, MMMM Do, YYYY ~ HH:mm"));
 
-
-//  when "submit" button is clicked
+//  when "submit" button is clicked 
 $("#submit").on("click", function(event) {
     //prevent page from refreshing
     event.preventDefault();
 
     //get values submitted in form
-    trainName = $("#train-name").val().trim();
-    destination = $("#train-destination").val().trim();
-    firstTrain = "#time-hr" + ":" + "#time-min";
-    frequency = Number.parseInt($("#train-frequency").val().trim());
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#train-destination").val().trim();
+    var firstTrain = $("#time-hr").val().trim() + ":" + $("#time-min").val().trim();
+    var frequency = Number.parseInt($("#train-frequency").val().trim());
 
-    // Setting values in the database
-    database.ref().set({  
+    var newTrain = {  
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency
-      });
-});
+      }
+    // Setting values in the database
+    database.ref().push(newTrain);
 
-//MISSING: child added and PUSH??????????????????
+      console.log(firstTrain);
+      
 
 // When changes occur Firebase will print them to console and html
-database.ref().on("value", function(snapshot) {  //WHAT IS "VALUE"????????????????????
+database.ref().on("child_added", function(childSnapshot) { 
 
     // Print the initial data to the console.
-    console.log(snapshot.val());
-  
-    // Log the value of the various properties
-    console.log(snapshot.val().trainName);
-    console.log(snapshot.val().destination);
-    console.log(snapshot.val().firstTrain);
-    console.log(snapshot.val().frequency);
-  
+    // console.log(childSnapshot.val());
+
+     // Log the value of the various properties
+    //console.log(childSnapshot.val().trainName);
+    // console.log(childSnapshot.val().destination);
+    // console.log(childSnapshot.val().firstTrain);
+   // console.log(childSnapshot.val().frequency);
+
+var postedName = childSnapshot.val().trainName;
+var postedDestination = childSnapshot.val().destination;
+var postedFrequency = childSnapshot.val().frequency;
+var fTrain = childSnapshot.val().firstTrain;
+
+
 //to calculate times:
 var currentTime = moment();
-    
+    console.log(moment().format("HH:mm"));
+var firstTime = moment(fTrain, "HH:mm");
+    console.log(moment(firstTime).format("HH:mm"));
 // Difference between the times
-var minutesElapsed = moment(currentTime).diff(firstTrain, "minutes");
-var minutesRemainder = minutesElapsed % frequency;
-minutesAway = frequency - minutesRemainder;
-nextArrival = moment().add(minutesAway, "minutes");
-
+var minutesElapsed = moment().diff(firstTime, "minutes");
+    console.log(minutesElapsed);
+var minutesRemainder = minutesElapsed % postedFrequency;
+var minutesAway = postedFrequency - minutesRemainder;
+    console.log(minutesAway);
+var nextArrival = moment(currentTime).add(minutesAway, "minutes");
+    console.log(moment(nextArrival).format("HH:mm"));
 
     // Change the HTML -- create table row
     $("#table").append(
-        "<tr><td>" + trainName + 
-        "</td><td>" + destination + 
-        "</td><td>" + frequency + 
-        "</td><td>" + nextArrival + 
-        "</td><td>" + MinutesAway + 
+        "<tr><td>" + postedName + 
+        "</td><td>" + postedDestination + 
+        "</td><td>" + postedFrequency + 
+        "</td><td>" + moment(nextArrival).format("HH:mm") + 
+        "</td><td>" + minutesAway + 
         "</td></tr>");
 
 // If any errors are experienced, log them to console.
@@ -88,7 +92,7 @@ nextArrival = moment().add(minutesAway, "minutes");
       console.log("The read failed: " + errorObject.code);
 });
   
-
+});
 
 
 //Future: on-click of "refresh-update" button call function postSchedule
